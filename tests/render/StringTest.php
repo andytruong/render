b8057a188e362233c1365d5e0cd269a5a5f5d5dc
Andy Truong <andy@toila.net>
2014-05-17 23:29:17 +0700

    + More test cases.

--- tests/render/StringTest.php
+++ tests/render/StringTest.php
@@ -6,8 +6,8 @@ class StringTest extends TestCase {
   /**
    * @dataProvider dataProvider
    */
-  public function testString($input, $msg = '') {
-    $this->assertEquals($input, $this->getRender()->render($input));
+  public function testString($input, $msg) {
+    $this->assertEquals($input, $this->getRender()->render($input), $msg);
   }
 
   public function dataProvider() {
@@ -15,6 +15,21 @@ class StringTest extends TestCase {
       array('Hello PHP', 'Basic ASCII string'),
       array('Xin chào ngôn ngữ PHP', 'Vietnamese string'),
       array('Hi PHP…', 'Special chars'),
+    );
+  }
+
+  /**
+   * @dataProvider negativeDataProvider
+   */
+  public function testNotEqual($unexpected, $input, $msg) {
+    $this->assertNotEquals($unexpected, $this->getRender()->render($input), $msg);
+  }
+
+  public function negativeDataProvider() {
+    return array(
+      array('Hello PHP!', 'Hello PHP', 'Basic ASCII string'),
+      array('Xin chào ngôn ngữ PHP!', 'Xin chào ngôn ngữ PHP', 'Vietnamese string'),
+      array('Hi PHP… ^^', 'Hi PHP…', 'Special chars'),
     );
   }
 }
